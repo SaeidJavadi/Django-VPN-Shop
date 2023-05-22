fc3f5d58918e5d6cda66a72ee2632e4bf7b037b4
Saeid Javadi <S.Javadi.P@Gmail.com>
2023-05-22 19:38:44 +0330

    fix select lvp from list selected action

--- vpn/static/vpn/js/buypg.js
+++ vpn/static/vpn/js/buypg.js
@@ -1,12 +1,8 @@
 var extra_wage = 3;
-var arrayids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 var discount_rate = 0;
 var $radios = $('input:radio[name=selected_service]');
 var buy_option_selected = 1;
-var time_expire = 0;
-var time_total = 0;
 
-var transid = "";
 
 function pf(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -15,14 +11,14 @@ if ($radios.is(':checked') === false) {
     $radios.filter('[value=' + buy_option_selected + ']').prop('checked', true);
     $('#trproduct_' + buy_option_selected).removeClass("bg-info").addClass("bg-info");
     if (extra_wage === 0) {
-        $("#finalprice").text(pf((i * (100.0 - discount_rate) / 100.0)) + " تومان");
+        $("#finalprice").text(pf((buy_option_selected * (100.0 - discount_rate) / 100.0)) + " تومان");
     } else {
-        $("#finalprice").text(pf((i * (100.0 - discount_rate + extra_wage) / 100.0)) + " تومان" + " ( " + extra_wage + " % کارمزد درگاه واسط)");
+        $("#finalprice").text(pf((buy_option_selected * (100.0 - discount_rate + extra_wage) / 100.0)) + " تومان" + " ( " + extra_wage + " % کارمزد درگاه واسط)");
     }
-    $("#finalpricetron").text(pf(i * 0.8) + " تومان");
+    $("#finalpricetron").text(pf(buy_option_selected * 0.8) + " تومان");
 }
 
-function reply_click(i) {
+function reply_click(i,t) {
     var idx;
     buy_option_selected = i;
     for (idx = 0; idx <= 10; idx++) {
@@ -33,10 +29,10 @@ function reply_click(i) {
     $('#trproduct_' + i).removeClass("bg-info").addClass("bg-info");
     $(".serviceselection").val(i);
     if (extra_wage === 0) {
-        $("#finalprice").text(pf((i * (100.0 - discount_rate) / 100.0)) + " تومان");
+        $("#finalprice").text(pf((t * (100.0 - discount_rate) / 100.0)) + " تومان");
     } else {
-        $("#finalprice").text(pf((i * (100.0 - discount_rate + extra_wage) / 100.0)) + " تومان" + " ( " + extra_wage + " % کارمزد درگاه واسط)");
+        $("#finalprice").text(pf((t * (100.0 - discount_rate + extra_wage) / 100.0)) + " تومان" + " ( " + extra_wage + " % کارمزد درگاه واسط)");
     }
 
-    $("#finalpricetron").text(pf(i * 0.8) + " تومان");
+    $("#finalpricetron").text(pf(t * 0.8) + " تومان");
 }
