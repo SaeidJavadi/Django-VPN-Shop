f94543874a8a747dcb912b8ca4d6326f41a9c716
Hiding <s.a.e.i.d@live.com>
2023-06-07 06:36:50 +0000

    up

--- vpn/templatetags/basetags.py
+++ vpn/templatetags/basetags.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.filter()
 def daytomo(value):
     return math.ceil(value / 31)
+
+
+@register.filter()
+def tocurrencys(value):
+    return str("{:,}".format(int(value)))
